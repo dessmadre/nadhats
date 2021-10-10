232a3103adc9d15ae5079c93b016157f2622f857
spacecoupe <iam@joseduarte.io>
2021-10-10 13:01:07 -0700

    Fix header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
@@ -8,7 +9,25 @@ const Header: React.FC = () => {
         alt='NadHats Banner Image'
         layout='fill'
         objectFit='cover'
-      ></Image>
+      />
+      <figure
+        className='absolute
+       w-full h-full header flex flex-col  items-start pl-150'
+      >
+        <figcaption className='mt-150'>
+          <h1 className='text-4xl font-semibold text-white leading-snug uppercase'>
+            Handmade Beanies
+          </h1>
+          <p className='text-white text-2xl font-medium'>
+            new drops every month
+          </p>
+          <Link href='/store' passHref>
+            <button className='bg-gray-800 hover:bg-black p-3 text-white text-xl font-semibold mt-5'>
+              Check out our collections
+            </button>
+          </Link>
+        </figcaption>
+      </figure>
     </header>
   );
 };
